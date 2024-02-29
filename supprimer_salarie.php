933e927d36a4a97a24c24109ff29e319ee49b5ea
champagnearden <jbbeck42@gmail.com>
2024-02-29 09:09:22 +0100

    delete system calls and create missing files

--- supprimer_salarie.php
+++ supprimer_salarie.php
@@ -5,8 +5,6 @@ session_start();
 $salaries = json_decode(file_get_contents("salariés.json"), true);
 foreach($salaries as $key => $salarie) {
     if($salarie['email'] == $_POST['email']) {
-        exec('sudo deluser '.$salarie['username']);
-        exec('sudo rm -R /var/www/sardines/intranet/FTP/UTILISATEURS/'.$salarie['username']);
         unset($salaries[$key]);
         break;
     }
