2c7dd19fdf9420c3450af42620777d750b78e3f6
champagnearden <jbbeck42@gmail.com>
2024-03-10 03:22:39 +0100

    added group choice when create directory or file

--- FTP/index.php
+++ FTP/index.php
@@ -35,14 +35,18 @@
   $_SESSION['path'] = $path;
   if(isset($_POST['AddDoss'])){
     $str=$_POST['AddDoss'];
+    $visi = $_POST['role'];
+    if ($visi == 'grp') {
+      $visi = $_POST['groups'];
+    }
     array_push($dossiers, array(
       "id" => uniqid(),
       "nom" => $str, 
       "path" => $path,
-      "visibility" => $_POST['role'], 
-      "owner" => $_SESSION['username'], 
+      "visibility" => $visi,
+      "owner" => $_SESSION['username'],
     ));
-      file_put_contents("../dossiers.json", json_encode($dossiers));
+    file_put_contents("../dossiers.json", json_encode($dossiers));
     unset($_POST['AddDoss']);
   }
   if (
@@ -53,34 +57,29 @@
     $str = str_replace('%22','',$_FILES['fic']['name']);
     $str = str_replace("'","",$str);
     move_uploaded_file($_FILES['fic']['tmp_name'],"./files/$str");
+    $visi = $_POST['role'];
+    if ($visi == 'grp') {
+      $visi = $_POST['groups'];
+    }
     array_push($fichiers, array(
       "id" => uniqid(),
       "nom" => $str, 
       "path" => $path,
-      "visibility" => $_POST['role'], 
+      "visibility" => $visi, 
       "owner" => $_SESSION['username']
     ));
     file_put_contents("../fichiers.json", json_encode($fichiers));
     unset($_FILES['fic']);
   }
   if(isset($_POST['delFic'])){
-    $size=sizeof($fichiers);
-    for($i=0;$i<$size;$i++){
-      if($fichiers[$i]['id']==$_POST['delFic']){
-        unset($fichiers[$i]);
-      }
-    }
-    file_put_contents("../fichiers.json", json_encode($fichiers));
+    $i = array_search($_POST["delFic"], array_column($fichiers, 'id'));
+    unset($fichiers[$i]);
+    file_put_contents("../fichiers.json", json_encode(array_values($fichiers)));
   }
   if(isset($_POST['delDir'])){
-    $sd=sizeof($dossiers);
-    for($i=0;$i<$sd;$i++){
-      if($dossiers[$i]['id']==$_POST['delDir']){
-        unset($dossiers[$i]);
-        break;
-      }
-    }
-    file_put_contents("../dossiers.json", json_encode($dossiers));
+    $i = array_search($_POST["delDir"], array_column($dossiers, 'id'));
+    unset($dossiers[$i]);
+    file_put_contents("../dossiers.json", json_encode(array_values($dossiers)));
   }
   echo "
   <div class='container textblue policesecond mediumsize'>
@@ -107,21 +106,24 @@
   $current_user = array_search($_SESSION["username"], array_column($salarie, 'username'));
   foreach ($dossiers as $d) {
     if ($d['path'] == $path) {
+      if ($d['owner'] == $_SESSION['username']) {
+        dispDir($d);
+        continue;
+      }
       switch ($d['visibility']) {
         case 'me':
           if ($d['owner']==$_SESSION['username']){
             dispDir($d);
           }
           break;
-        case 'grp':
-          $owner = array_search($d['owner'], array_column($salarie, 'username'));
-          $common = array_intersect($salarie[$owner]['groupe'], $salarie[$current_user]['groupe']);
+        case 'all':
+          dispDir($d);
+          break;
+        default:
+          $common = array_intersect($d['visibility'], $salarie[$current_user]['groupe']);
           if(sizeof($common) > 0){
             dispDir($d);
           }
-          break;
-        case 'all':
-          dispDir($d);
           break;
       }
     }
@@ -159,9 +161,9 @@
     $condition = $const['FTP']['CONFIRM_FILE'].$file['nom'];
     echo "
     <div class='raw'>
-      <form action='' method='post' id='delfile' class='inline'>
+      <form action='' method='post' id='delfile_".$file['id']."' class='inline'>
         <input type='hidden' name='delFic' value='".$file['id']."'>
-        <a class='btn bgmaincolor text-white btn-sm' onclick=\"confirmDelete('$condition', 'delfile');\">-</a>
+        <a class='btn bgmaincolor text-white btn-sm' onclick=\"confirmDelete('$condition', 'delfile_".$file['id']."');\">-</a>
       </form>
       &emsp;
       <a class='textblue policesecond minisize' href='./".$file['nom']."' download='".$file['nom']."'>".$file['nom']."</a> 
@@ -178,22 +180,25 @@
   ";
   foreach ($fichiers as $f) {
     if ($f ['path'] == $path) {
+      if ($f['owner'] == $_SESSION['username']) {
+        dispFile($f);
+        continue;
+      }
       switch ($f['visibility']) {
         case 'me':
           if ($f['owner']==$_SESSION['username']) {
             dispFile($f);
           }
           break;
-        case 'grp':
-          $owner = array_search($f['owner'], array_column($salarie, 'username'));
-          $common = array_intersect($salarie[$owner]['groupe'], $salarie[$current_user]['groupe']);
-          if(sizeof($common) > 0){
-            dispFile($f);
-          }
-          break;
         case 'all':
           dispFile($f);
           break;
+        default:
+          $common = array_intersect($f['visibility'], $salarie[$current_user]['groupe']);
+          if(sizeof($common) > 0){
+            dispFile($f);
+          }
+        break;
       }
     }
   }
@@ -210,39 +215,69 @@
 </div><br>
 <hr class='container textblue'><br>
 <div class='mx-auto d-block textblue '>
-  <form action="" method="post">
-    <small><i><?php echo $const['FTP']['NO_QUOTES']; ?></i></small>
-    <br><br>
-    <div class='container row'>
-      <div class='col-sm-6'>
-        <p>&emsp;<?php echo $const['FTP']['CREATE_FOLDER']; ?></p>
-        <div class='textblue'>
+  <small><i><?php echo $const['FTP']['NO_QUOTES']; ?></i></small>
+  <br><br>
+  <div class='container row'>
+    <div class='col-sm-6'>
+      <p>&emsp;<?php echo $const['FTP']['CREATE_FOLDER']; ?></p>
+      <div class='textblue'>
+        <form action="" method="post">
           <input class='textblue minisize policesecond' style='border-radius: 2%' type="text" name="AddDoss" placeholder="<?php echo $const['FTP']['FOLDER_NAME']; ?>" required>
           <p>&emsp;<?php echo $const['FTP']['WHO']; ?></p>
-          <select id="role" name="role">
+          <select id="role_dir" name="role" onchange="revealGroups(this, 'dir')">
             <option value="me"><?php echo $const['FTP']['ONLY_ME']; ?></option>
             <option value="grp"><?php echo $const['FTP']['GROUPS']; ?></option>
             <option value="all"><?php echo $const['FTP']['ALL']; ?></option>
           </select>
-          <br><br>
+          <br>
+          <span id='groups_dir_span'></span>
+          <br>
           <button class='btn text-white smallsize bgmaincolor' type="submit" value='Créer le dossier'><?php echo $const['FTP']['CREATE']; ?></button>
-  </form>
-        </div>
-      </div>
-      <div class='col-sm-6'>
-        <p><?php echo $const['FTP']['UPLOAD']; ?></p>
-        <form action="./" method="post" enctype="multipart/form-data">
-          <input class='textblue minisize policesecond' style='border-radius: 2%' type="file" name="fic">
-          <p>&emsp;<?php echo $const['FTP']['WHO']; ?></p>
-            <select id="role" name="role">
-              <option value="me"><?php echo $const['FTP']['ONLY_ME']; ?></option>
-              <option value="grp"><?php echo $const['FTP']['GROUPS']; ?></option>
-              <option value="all"><?php echo $const['FTP']['ALL']; ?></option>
-            </select>
-          <br><br>
-          <button class='btn text-white bgmaincolor policesecond' type="submit" value="Importer le fichier"><?php echo $const['FTP']['UPLOAD']; ?></button>
         </form>
+      </div>
+    </div>
+    <div class='col-sm-6'>
+      <p><?php echo $const['FTP']['UPLOAD']; ?></p>
+      <form action="./" method="post" enctype="multipart/form-data">
+        <input class='textblue minisize policesecond' style='border-radius: 2%' type="file" name="fic">
+        <p>&emsp;<?php echo $const['FTP']['WHO']; ?></p>
+        <select id="role_fic" name="role" onchange="revealGroups(this, 'fic')">
+          <option value="me"><?php echo $const['FTP']['ONLY_ME']; ?></option>
+          <option value="grp"><?php echo $const['FTP']['GROUPS']; ?></option>
+          <option value="all"><?php echo $const['FTP']['ALL']; ?></option>
+        </select>
         <br>
+        <span id="groups_fic_span"></span>
+        <br>
+        <button class='btn text-white bgmaincolor policesecond' type="submit" value="Importer le fichier"><?php echo $const['FTP']['UPLOAD']; ?></button>
+      </form>
+      <script>
+        function revealGroups(self, id) {
+          if (self.value == 'grp') {
+            const span = document.getElementById('groups_' + id + '_span');
+            const select = document.createElement('select');
+            const width =  self.getBoundingClientRect()['width'] + 'px'
+            span.style.width = width ;
+            select.style.width = width;
+            select.multiple = true;
+            select.name = 'groups[]';
+            select.id = 'groups_' + id;
+            <?php
+              $current_user = array_search($_SESSION["username"], array_column($salarie, 'username'));
+              foreach ($salarie[$current_user]['groupe'] as $g) {
+                echo "select.options.add(new Option('$g', '$g', false, true));";
+              }
+            ?>
+            span.appendChild(select);
+          } else {
+            const select = document.getElementById('groups_' + id)
+            if (select) {
+              select.remove();
+            }
+          }
+        } 
+      </script>
+      <br>
       </div>
     </div>
   </div>
